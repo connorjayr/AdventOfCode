8a9e0f156a76f437c2bdc4c8362db04422e20c78
Connor Roizman <connor@roizman.org>
2021-12-06 21:31:44 -0500

    Add color to warning and error messages

--- launcher.py
+++ launcher.py
@@ -3,18 +3,56 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import argparse
+import bs4
 import clipboard
 import datetime
 import importlib
 import os
 import pathlib
+import re
 import requests
 import sys
-from typing import Optional
+from termcolor import colored
+from typing import Optional, Tuple
+
+
+def retrieve_example(day: int, year: int) -> str:
+    """Retrieves the example input for a puzzle from https://adventofcode.com.
+
+    Args:
+        day: Which day the puzzle is from.
+        year: Which year the puzzle is from.
+
+    Returns:
+        The example puzzle input.
+    """
+    # Attempt to retrieve the example puzzle input from https://adventofcode.com
+    url = f"https://adventofcode.com/{year}/day/{day}"
+    response = requests.get(url)
+
+    doc = bs4.BeautifulSoup(response.text, "html.parser")
+    keywords = ["for example", "example", "suppose"]
+    example: Optional[str] = None
+    for keyword in keywords:
+        match = doc.find(text=re.compile(keyword))
+        if match is not None:
+            code = match.parent.find_next("code")
+            if code is not None:
+                example = code.text.strip()
+                break
+    if example is None:
+        print(
+            colored("ERROR", "red", attrs=["bold"]),
+            "Cannot find example puzzle input, aborting",
+            file=sys.stderr,
+        )
+        exit(1)
+
+    return example
 
 
 def retrieve_input(day: int, year: int) -> Optional[str]:
-    """Retrieves a puzzle's input from https://adventofcode.com.
+    """Retrieves the input for a puzzle from https://adventofcode.com.
 
     Args:
         day: Which day the puzzle is from.
@@ -25,7 +63,9 @@ def retrieve_input(day: int, year: int) -> Optional[str]:
     """
     # If the puzzle input has previously been retrieved, then read it from the
     # file
-    input_path = pathlib.Path(f"inputs/year{year}/day{str(day).rjust(2, '0')}.txt")
+    input_path = pathlib.Path(
+        f"inputs/year{year}/day{str(day).rjust(2, '0')}/input.txt"
+    )
     if input_path.exists():
         with open(input_path, "r") as input_file:
             return input_file.read()
@@ -35,7 +75,11 @@ def retrieve_input(day: int, year: int) -> Optional[str]:
     session = os.getenv("ADVENT_OF_CODE_SESSION")
     response = requests.get(url, cookies={"session": session})
     if response.status_code != 200:
-        print(f"cannot retrieve input from {url}", file=sys.stderr)
+        print(
+            colored("WARNING", "yellow", attrs=["bold"]),
+            f"Cannot retrieve input from {url}",
+            file=sys.stderr,
+        )
         return None
     input = response.text.strip()
 
@@ -63,9 +107,30 @@ def main():
         help="which year the puzzle is from",
         type=int,
     )
-    parser.add_argument(
+
+    # A user should not be able to do more than one of the following at the same
+    # time:
+    #
+    # -
+    input_group = parser.add_mutually_exclusive_group()
+    input_group.add_argument(
+        "--example",
+        "-e",
+        action="store_true",
+        default=False,
+        help="attempts to use the puzzle's example input",
+    )
+    input_group.add_argument(
         "--input", "-i", help="path to the puzzle input", type=argparse.FileType("r")
     )
+    input_group.add_argument(
+        "--submit",
+        "-s",
+        action="store_true",
+        default=False,
+        help="submits the solution(s)",
+    )
+
     parser.add_argument(
         "--part",
         "-p",
@@ -80,13 +145,21 @@ def main():
         day = str(args.day).rjust(2, "0")
         solver = importlib.import_module(f"solvers.year{args.year}.day{day}.solver")
     except ModuleNotFoundError:
-        print(f"No solver for day {args.day}")
+        print(
+            colored("ERROR", "red", attrs=["bold"]),
+            f"No solver for day {args.day}",
+            file=sys.stderr,
+        )
         exit(1)
 
     parts = set(args.part or [])
-    input = (
-        retrieve_input(args.day, args.year) if args.input is None else args.input.read()
-    )
+    doc: Optional[bs4.BeautifulSoup] = None
+    if args.example:
+        input = retrieve_example(args.day, args.year)
+    elif args.input is not None:
+        input = args.input.read()
+    else:
+        input = retrieve_input(args.day, args.year)
     for (part, solution) in enumerate(solver.solve(input), 1):
         if len(parts) == 0 or part in parts:
             print(f"Part {part} solution: {solution}")
