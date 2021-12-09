f9415433b43fd88bc857a714a1da59f4ad9034a0
Connor Roizman <connor@roizman.org>
2021-12-09 15:40:55 -0500

    Add solver for day 7 of 2021

--- solvers/year2021/day07/solver.py
+++ solvers/year2021/day07/solver.py
@@ -1,7 +1,13 @@
 from typing import Iterator, Optional
 from util import *
+import math
 
 
 def solve(input: Optional[str]) -> Iterator[any]:
-    yield "Part 1 solution"
-    yield "Part 2 solution"
+    positions = [int(pos) for pos in input.split(",")]
+    pos_range = range(min(positions), max(positions) + 1)
+    yield min(sum(abs(pos - align_pos) for pos in positions) for align_pos in pos_range)
+    yield min(
+        sum(abs(pos - align_pos) * (abs(pos - align_pos) + 1) // 2 for pos in positions)
+        for align_pos in pos_range
+    )
