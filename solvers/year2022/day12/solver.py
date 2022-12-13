9ff1b34ce8ad56883542856f3ddf20189234c146
Connor Roizman <connor@roizman.org>
2022-12-12 22:44:26 -0500

    Refactor day 12 solver

--- solvers/year2022/day12/solver.py
+++ solvers/year2022/day12/solver.py
@@ -7,7 +7,8 @@ def bfs(grid: list[str], starts: list[tuple[int, int]]) -> int:
     queue = [(0, pos) for pos in starts]
     while len(queue) > 0:
         steps, pos = queue.pop(0)
-        if grid[pos[0]][pos[1]] == "E":
+        elev = grid[pos[0]][pos[1]]
+        if elev == "E":
             return steps
         for d in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             new_pos = (pos[0] + d[0], pos[1] + d[1])
@@ -20,13 +21,12 @@ def bfs(grid: list[str], starts: list[tuple[int, int]]) -> int:
                 continue
             if new_pos in visited:
                 continue
-            old_elev = grid[pos[0]][pos[1]]
-            if old_elev == "S":
-                old_elev = "a"
+            if elev == "S":
+                elev = "a"
             new_elev = grid[new_pos[0]][new_pos[1]]
             if new_elev == "E":
                 new_elev = "z"
-            if ord(new_elev) - ord(old_elev) <= 1:
+            if ord(new_elev) - ord(elev) <= 1:
                 visited.add(new_pos)
                 queue.append((steps + 1, new_pos))
 
