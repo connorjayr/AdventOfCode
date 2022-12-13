ba466937345df2201115bb649ff237d71a56911d
Connor Roizman <connor@roizman.org>
2022-12-12 22:51:20 -0500

    Fix solver for day 3 of 2022

--- solvers/year2022/day03/solver.py
+++ solvers/year2022/day03/solver.py
@@ -4,18 +4,23 @@ from util import *
 
 def solve(input: Optional[str]) -> Iterator[any]:
     sacks = input.split("\n")
-    sumt = 0
-    for i in range(0, len(sacks), 3):
-        one = sacks[i]
-        two = sacks[i + 1]
-        thr = sacks[i + 2]
-        # left = set(sack[: len(sack) // 2])
-        # right = set(sack[len(sack) // 2 :])
-        shared = set(one).intersection(set(two).intersection(set(thr)))
-        shared = ord(next(iter(shared)))
-        if ord("a") <= shared <= ord("z"):
-            sumt += shared - ord("a") + 1
+    sum_p1 = 0
+    sum_p2 = 0
+    for idx in range(len(sacks)):
+        sack = sacks[idx]
+        shared = next(iter(set(sack[: len(sack) // 2]) & set(sack[len(sack) // 2 :])))
+        if shared.isupper():
+            sum_p1 += ord(shared) - ord("A") + 27
         else:
-            sumt += shared - ord("A") + 27
-    yield 0
-    yield sumt
+            sum_p1 += ord(shared) - ord("a") + 1
+
+        if idx % 3 == 0:
+            shared = next(
+                iter(set(sacks[idx]) & set(sacks[idx + 1]) & set(sacks[idx + 2]))
+            )
+            if shared.isupper():
+                sum_p2 += ord(shared) - ord("A") + 27
+            else:
+                sum_p2 += ord(shared) - ord("a") + 1
+    yield sum_p1
+    yield sum_p2
